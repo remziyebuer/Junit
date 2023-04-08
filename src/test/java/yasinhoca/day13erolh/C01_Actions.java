9aed5dc6837e7b3eecd5985a7a0e3f3245dabe62
isminiz <115974069+remziyebuer@users.noreply.github.com>
2023-04-09 00:38:15 +0200

    ...

--- src/test/java/yasinhoca/day13erolh/C01_Actions.java
+++ src/test/java/yasinhoca/day13erolh/C01_Actions.java
@@ -1,36 +1,43 @@
 package yasinhoca.day13erolh;
-
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
 import yasinhoca.utilities.TestBase;
 
 public class C01_Actions extends TestBase {
-
      /*
-    Amazon anasayfasına gidin
-    "Account & List" üzerinde bekleyin (mouse over - hover over)
-    Açılan menüde "Account" linkine tıklayın
-    Açılan sayfanın başlığının "Your Account" içerdiğini doğrulayın
+    https://the-internet.herokuapp.com/context_menu  sitesine gidin
+    Kutuya sağ tıklayın
+    Alert’te cikan yazinin “You selected a context menu” oldugunu test edin
+    Tamam diyerek alert’i kapatın
      */
 
     @Test
     public void actionsTest() {
-//        Amazon anasayfasına gidin
-        driver.get("https://www.amazon.com");
+//        https://the-internet.herokuapp.com/context_menu  sitesine gidin
+        driver.get("https://the-internet.herokuapp.com/context_menu");
 
-//        "Account & List" üzerinde bekleyin (mouse over - hover over)
-        WebElement accountAndList = driver.findElement(By.id("nav-link-accountList"));
+//        Kutuya sağ tıklayın
+        //Sağ tık işlemi için contextClick() methoduna ihtiyacımız var. Bunun için Actions objesi oluşturulmalıdır.
 
+        //1. adım: Actions objesi oluşturun
         Actions actions = new Actions(driver);
-        actions.moveToElement(accountAndList).perform();//moveToElement() methodu "hover over" işlemini gerçekleştirir.
 
-//        Açılan menüde "Account" linkine tıklayın
-        driver.findElement(By.linkText("Account")).click();
+        //2. adım: Üzerinde işlem yapılacak WebElement'i locate edin
+        WebElement kutu = driver.findElement(By.id("hot-spot"));
 
-//        Açılan sayfanın başlığının "Your Account" içerdiğini doğrulayın
-        String sayfaBasligi = driver.getTitle();
-        assertEquals("Your Account", sayfaBasligi);
+        //3. adım: İşlemi uygulayın
+        actions.contextClick(kutu).perform();//perform() methodu son olarak uygulanmalıdır.
+
+//        Alert’te cikan yazinin “You selected a context menu” oldugunu test edin
+        String alertYazisi = driver.switchTo().alert().getText();
+       Assert.assertEquals("You selected a context menu", alertYazisi);
+
+//        Tamam diyerek alert’i kapatın
+        driver.switchTo().alert().accept();
 
     }
-
 }
--- src/test/java/yasinhoca/day13erolh/C02_Actions.java
+++ src/test/java/yasinhoca/day13erolh/C02_Actions.java
@@ -1,38 +1,39 @@
 package yasinhoca.day13erolh;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import yasinhoca.utilities.TestBase;
 
 public class C02_Actions extends TestBase {
+
      /*
-    https://the-internet.herokuapp.com/context_menu  sitesine gidin
-    Kutuya sağ tıklayın
-    Alert’te cikan yazinin “You selected a context menu” oldugunu test edin
-    Tamam diyerek alert’i kapatın
+    Amazon anasayfasına gidin
+    "Account & List" üzerinde bekleyin (mouse over - hover over)
+    Açılan menüde "Account" linkine tıklayın
+    Açılan sayfanın başlığının "Your Account" içerdiğini doğrulayın
      */
 
     @Test
     public void actionsTest() {
-//        https://the-internet.herokuapp.com/context_menu  sitesine gidin
-        driver.get("https://the-internet.herokuapp.com/context_menu");
+//        Amazon anasayfasına gidin
+        driver.get("https://www.amazon.com");
 
-//        Kutuya sağ tıklayın
-        //Sağ tık işlemi için contextClick() methoduna ihtiyacımız var. Bunun için Actions objesi oluşturulmalıdır.
+//        "Account & List" üzerinde bekleyin (mouse over - hover over)
+        WebElement accountAndList = driver.findElement(By.id("nav-link-accountList"));
 
-        //1. adım: Actions objesi oluşturun
         Actions actions = new Actions(driver);
+        actions.moveToElement(accountAndList).perform();//moveToElement() methodu "hover over" işlemini gerçekleştirir.
 
-        //2. adım: Üzerinde işlem yapılacak WebElement'i locate edin
-        WebElement kutu = driver.findElement(By.id("hot-spot"));
+//        Açılan menüde "Account" linkine tıklayın
+        driver.findElement(By.linkText("Account")).click();
 
-        //3. adım: İşlemi uygulayın
-        actions.contextClick(kutu).perform();//perform() methodu son olarak uygulanmalıdır.
-
-//        Alert’te cikan yazinin “You selected a context menu” oldugunu test edin
-        String alertYazisi = driver.switchTo().alert().getText();
-        assertEquals("You selected a context menu", alertYazisi);
-
-//        Tamam diyerek alert’i kapatın
-        driver.switchTo().alert().accept();
+//        Açılan sayfanın başlığının "Your Account" içerdiğini doğrulayın
+        String sayfaBasligi = driver.getTitle();
+        Assert.assertEquals("Your Account", sayfaBasligi);
 
     }
+
 }
