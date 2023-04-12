1c6bc0227bcd67a49d9782e02c919ff197e1f0e1
isminiz <115974069+remziyebuer@users.noreply.github.com>
2023-04-12 14:43:28 +0200

    ...

---
+++ src/test/java/yasinhoca/day15erolh/C02_ExplicitWait.java
@@ -0,0 +1,19 @@
+package yasinhoca.day15erolh;
+
+
+import org.junit.Test;
+import yasinhoca.utilities.TestBase;
+
+public class C02_ExplicitWait extends TestBase {
+    /*
+    https://the-internet.herokuapp.com/dynamic_loading/1
+    Start buttonuna tıklayın
+    Hello World! Yazının sitede oldugunu test edin
+     */
+
+    @Test
+    public void explicitWait() {
+
+
+    }
+}
