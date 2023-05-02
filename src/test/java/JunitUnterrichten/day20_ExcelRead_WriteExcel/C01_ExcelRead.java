f54d49cf9ead044dbdf34eb36727fe858f51717a
isminiz <115974069+remziyebuer@users.noreply.github.com>
2023-05-02 21:14:56 +0200

    ...

---
+++ src/test/java/JunitUnterrichten/day20_ExcelRead_WriteExcel/C01_ExcelRead.java
@@ -0,0 +1,10 @@
+package JunitUnterrichten.day20_ExcelRead_WriteExcel;
+
+public class C01_ExcelRead {
+    //pom xml e apachi poi ekleyerek excel  datamizi okuyabiliyor olucaz
+    //örn 1000 tane kullanici ad ve sifre var tercihen excelde saklayip
+    // kullanacagiz
+    //öncelikle dosyayi disardan alip okuyacak File Input Steream (dosyayi iceri alan )
+    // classina ihtiyacimiz var.Birde File Output Stream var ama onu burda kullanmayacagiz
+    //daha sonra onu okumak icin Workbook clasina ihtiyacimiz olacak
+}
